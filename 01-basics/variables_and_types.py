60ecc5f0bbadd22d303ce48194cfdda92bd75e20
christoffertoft13 <chrtof13@gmail.com>
2026-01-22 18:40:50 +0100

    Variables and data types

---
+++ 01-basics/variables_and_types.py
@@ -0,0 +1,13 @@
+# Variables and data types
+
+name = "christoffer"
+age = 21
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Is student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(is_student))
